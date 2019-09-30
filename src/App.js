3a17226d32c93afee17f354f51e5f7b2321aeb1f
christopher.hogg <christopher.hogg@blik.io>
2019-09-30 19:28:28 +0200

    feat: get an example of a timer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
 import { useStopwatch } from 'react-timer-hook';
-
+import Timer from 'react-compound-timer'
 
 function App() {
 
@@ -16,6 +16,11 @@ function App() {
     pause,
     reset,
   } = useStopwatch({ autoStart: false });
+  var startTime = Date.now();
+  var interval = setInterval(function() {
+    var elapsedTime = Date.now() - startTime;
+    document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(3);
+}, 100);
   return (
     <section className="section">
     <div className="container">
@@ -34,9 +39,45 @@ function App() {
       <button onClick={pause}>Pause</button>
       <button onClick={reset}>Reset</button>
       <button onMouseDown={start} onMouseUp={pause}>Start</button>
+      <span id="timer"></span> s
 
     </div>
     </div>
+
+    <div className="container">
+    <Timer
+    initialTime={55000}
+    startImmediately={false}
+    onStart={() => console.log('onStart hook')}
+    onResume={() => console.log('onResume hook')}
+    onPause={() => console.log('onPause hook')}
+    onStop={() => console.log('onStop hook')}
+    onReset={() => console.log('onReset hook')}
+>
+    {({ start, resume, pause, stop, reset, timerState }) => (
+        <React.Fragment>
+            <div>
+                <Timer.Days /> days
+                <Timer.Hours /> hours
+                <Timer.Minutes /> minutes
+                <Timer.Seconds /> seconds
+                <Timer.Milliseconds /> milliseconds
+            </div>
+            <br />
+            <div>
+                <button onClick={start}>Start</button>
+                <button onClick={pause}>Pause</button>
+                <button onClick={resume}>Resume</button>
+                <button onClick={stop}>Stop</button>
+                <button onClick={reset}>Reset</button>
+            </div>
+        </React.Fragment>
+    )}
+</Timer>
+
+    </div>
+
+
   </section>
   );
 }
