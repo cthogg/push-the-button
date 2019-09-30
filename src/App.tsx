1f4c2d110e4b9d08b1c4e4d9ec9a73f0b80ba232
christopher.hogg <christopher.hogg@blik.io>
2019-09-30 18:51:55 +0200

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <section className="section">
+    <div className="container">
+      <h1 className="title">Hello World</h1>
+      <p className="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
+  );
+}
+
+export default App;
