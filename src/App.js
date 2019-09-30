79302ab8c9f4cd348aa4b1f96f89ee84e04e833d
christopher.hogg <christopher.hogg@blik.io>
2019-09-30 19:10:16 +0200

    feat: push the button on mouse up and pause

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import React from "react";
 import "./App.css";
 import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
+import { useStopwatch } from 'react-timer-hook';
 
 
 function App() {
+
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    start,
+    pause,
+    reset,
+  } = useStopwatch({ autoStart: false });
   return (
     <section className="section">
     <div className="container">
       <h1 className="title">Press the Button</h1>
-      <AwesomeButton type="primary">Button</AwesomeButton>
+      <AwesomeButton onMouseDown={start} onMouseUp={pause} type="primary">Button</AwesomeButton>
+    </div>
+
+    <div className="container">
+    <div style={{textAlign: 'center'}}>
+      <h1>react-timer-hook</h1>
+      <p>Stopwatch Demo</p>
+      <div style={{fontSize: '100px'}}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+      </div>
+      <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={reset}>Reset</button>
+      <button onMouseDown={start} onMouseUp={pause}>Start</button>
+
+    </div>
     </div>
   </section>
   );
