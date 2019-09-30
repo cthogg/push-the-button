3987cccd49db577bfd1b55294cb1bb70f2bc0634
christopher.hogg <christopher.hogg@blik.io>
2019-09-30 20:08:51 +0200

    feat: simpler way of getting the date

--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import "react-awesome-button/dist/styles.css";
 import "./App.css";
 
 function App() {
-  const [date, setDate] = React.useState(new Date());
+  const [startDate, setStartDate] = React.useState(new Date());
+  const [endDate, setEndDate] = React.useState(new Date());
 
-  //Replaces componentDidMount and componentWillUnmount
-  React.useEffect(() => {
-    var timerID = setInterval(() => tick(), 10);
-    return function cleanup() {
-      clearInterval(timerID);
-    };
-  });
-
-  function tick() {
-    setDate(new Date());
-  }
 
   return (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {date.getSeconds()} {date.getMilliseconds()}.</h2>
+      <button onClick={()=> setStartDate(new Date())}> SET DATE </button>
+      <button onClick={()=> setEndDate(new Date())} > END DATE </button>
+
+      <h2>It is {startDate.getSeconds()} {startDate.getMilliseconds()}.</h2>
+
+      <h2>It is {endDate.getSeconds()} {endDate.getMilliseconds()}.</h2>
+
     </div>
   );
 }
